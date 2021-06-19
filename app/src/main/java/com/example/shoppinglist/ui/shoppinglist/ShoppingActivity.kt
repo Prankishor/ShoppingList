6834474d88cb082ecd201ad85f6f9c0ae3573137
Prankishor <33565966+Prankishor@users.noreply.github.com>
2021-06-19 10:46:13 +0530

    before resolving error

--- app/src/main/java/com/example/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/example/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -2,20 +2,47 @@ package com.example.shoppinglist.ui.shoppinglist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.shoppinglist.R
+import com.example.shoppinglist.data.db.entities.ShoppingItem
+import com.example.shoppinglist.databinding.ActivityShoppingBinding
+import com.example.shoppinglist.other.ShoppingItemAdapter
 
 class ShoppingActivity : AppCompatActivity() {
 
     private lateinit var viewModel : ShoppingViewModel
 
+    private lateinit var binding : ActivityShoppingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
 
+        binding = ActivityShoppingBinding.inflate(layoutInflater)
+
         viewModel = ViewModelProvider(this).get(ShoppingViewModel::class.java)
 
+        val adapter = ShoppingItemAdapter(listOf(), viewModel)
 
+        binding.rvShoppingItems.layoutManager = LinearLayoutManager(this)
+
+        binding.rvShoppingItems.adapter = adapter
+
+        viewModel.getAllShoppingItems().observe(this, Observer {
+            adapter.items = it
+            adapter.notifyDataSetChanged()
+        })
+
+        binding.fab.setOnClickListener{
+            AddShoppingItemDialog(this,
+            object : AddDialogListener{
+                override fun onAddButtonClicked(item: ShoppingItem) {
+                    viewModel.upsert(item)
+                }
+            }).show()
+        }
 
     }
 }
